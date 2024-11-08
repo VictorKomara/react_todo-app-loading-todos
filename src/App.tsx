87647787db81dfbf210cdf590af11f828897c8d0
Victor Komara <vitioklss@gmail.com>
2024-11-08 16:18:22 +0200

    add task next solution

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,10 @@ import { UserWarning } from './UserWarning';
 import { getTodos, USER_ID } from './api/todos';
 import { Todo } from './types/Todo';
 import classNames from 'classnames';
-
-type SelectedFilter = 'All' | 'Active' | 'Completed';
+import { Footer } from './components/Footer';
+import { SelectedFilter } from './types/SelectedFilter';
+import { Header } from './components/Header';
+import { TodoList } from './components/TodoList';
 
 export const App: React.FC = () => {
   if (!USER_ID) {
@@ -16,22 +18,22 @@ export const App: React.FC = () => {
 
   const [todos, setTodos] = useState<Todo[]>([]);
   const [errorLoad, setErrorLoad] = useState(false);
-  const [selectedFilter, setSelectedFilter] = useState<SelectedFilter>('All');
+  const [selectedFilter, setSelectedFilter] = useState<SelectedFilter>(
+    SelectedFilter.all,
+  );
 
   const visibleTodos = todos.filter(todo => {
     switch (selectedFilter) {
-      case 'All':
+      case SelectedFilter.all:
         return true;
-      case 'Active':
-        return todo.completed === false;
-      case 'Completed':
-        return todo.completed === true;
+      case SelectedFilter.active:
+        return !todo.completed;
+      case SelectedFilter.completed:
+        return todo.completed;
     }
   });
 
-  const itemLeft: number = todos.filter(
-    todo => todo.completed === false,
-  ).length;
+  const itemLeft: number = todos.filter(todo => !todo.completed).length;
 
   function getAllTodos() {
     getTodos()
@@ -56,125 +58,16 @@ export const App: React.FC = () => {
       <h1 className="todoapp__title">todos</h1>
 
       <div className="todoapp__content">
-        <header className="todoapp__header">
-          {/* this button should have `active` class only if all todos are completed */}
-          <button
-            type="button"
-            className="todoapp__toggle-all active"
-            data-cy="ToggleAllButton"
+        <Header />
+
+        {todos.length > 0 && <TodoList visibleTodos={visibleTodos} />}
+
+        {todos.length > 0 && (
+          <Footer
+            itemLeft={itemLeft}
+            selectedFilter={selectedFilter}
+            setSelectedFilter={setSelectedFilter}
           />
-
-          {/* Add a todo on form submit */}
-          <form>
-            <input
-              data-cy="NewTodoField"
-              type="text"
-              className="todoapp__new-todo"
-              placeholder="What needs to be done?"
-            />
-          </form>
-        </header>
-
-        {todos.length > 0 && (
-          <section className="todoapp__main" data-cy="TodoList">
-            {visibleTodos.map(todo => (
-              <div
-                data-cy="Todo"
-                className={classNames('todo', { completed: todo.completed })}
-                key={todo.id}
-              >
-                <label className="todo__status-label">
-                  {todo.completed ? (
-                    <input
-                      data-cy="TodoStatus"
-                      type="checkbox"
-                      className="todo__status"
-                      checked
-                    />
-                  ) : (
-                    <input
-                      data-cy="TodoStatus"
-                      type="checkbox"
-                      className="todo__status"
-                    />
-                  )}
-                </label>
-
-                <span data-cy="TodoTitle" className="todo__title">
-                  {todo.title}
-                </span>
-
-                {/* Remove button appears only on hover */}
-                <button
-                  type="button"
-                  className="todo__remove"
-                  data-cy="TodoDelete"
-                >
-                  x
-                </button>
-
-                {/* overlay will cover the todo while it is being deleted or updated */}
-                <div data-cy="TodoLoader" className="modal overlay">
-                  <div className="modal-background has-background-white-ter" />
-                  <div className="loader" />
-                </div>
-              </div>
-            ))}
-          </section>
-        )}
-
-        {/* Hide the footer if there are no todos */}
-        {todos.length > 0 && (
-          <footer className="todoapp__footer" data-cy="Footer">
-            <span className="todo-count" data-cy="TodosCounter">
-              {itemLeft} items left
-            </span>
-
-            {/* Active link should have the 'selected' class */}
-            <nav className="filter" data-cy="Filter">
-              <a
-                href="#/"
-                className={classNames('filter__link', {
-                  selected: selectedFilter === 'All',
-                })}
-                data-cy="FilterLinkAll"
-                onClick={() => setSelectedFilter('All')}
-              >
-                All
-              </a>
-
-              <a
-                href="#/active"
-                className={classNames('filter__link', {
-                  selected: selectedFilter === 'Active',
-                })}
-                data-cy="FilterLinkActive"
-                onClick={() => setSelectedFilter('Active')}
-              >
-                Active
-              </a>
-
-              <a
-                href="#/completed"
-                className={classNames('filter__link', {
-                  selected: selectedFilter === 'Completed',
-                })}
-                data-cy="FilterLinkCompleted"
-                onClick={() => setSelectedFilter('Completed')}
-              >
-                Completed
-              </a>
-            </nav>
-
-            {/* this button should be disabled if there are no completed todos */}
-            <button
-              type="button"
-              className="todoapp__clear-completed"
-              data-cy="ClearCompletedButton"
-            >
-              Clear completed
-            </button>
-          </footer>
         )}
       </div>
 
